b1ba0b5eb23f06790e46353aa6fc4952046e2e7d
altanlabs <interfaces@altan.ai>
2025-02-08 19:52:13 +0000

    New commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import './App.css'
 import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import IndexPage from './pages/index'
+import PlaylistPage from './pages/playlist'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<IndexPage />} />
+          <Route path="playlist/:id" element={<PlaylistPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
